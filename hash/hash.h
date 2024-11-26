9df40650e3735a48445e41fb3a32e1fc0fd3b4b4
Wu-xingjie <1223500013@qq.com>
2024-11-27 00:42:14 +0800

    <fix>完善了哈希表的代码

--- hash/hash.h
+++ hash/hash.h
@@ -4,22 +4,22 @@
 namespace __HASH {
 template <typename T>
 struct Data {
-  Data() : _forward(nullptr), _back(nullptr) {}
-  Data(const int& key, const T& value) : _forward(nullptr), _back(nullptr) {
+  Data() : _before(nullptr), _next(nullptr) {}
+  Data(const int& key, const T& value) : _before(nullptr), _next(nullptr) {
     _key = key;
     _value = value;
   }
   int _key;
   T _value;
-  Data* _forward;
-  Data* _back;
+  Data* _before;
+  Data* _next;
 };
 
 template <typename T>
 class hash {
  public:
   hash(const int& size = 10, const int& base = 5) : _size(size), _base(base) {
-    hashtable_ = new Data<T>[_size];
+    hashtable_ = new Data<T>*[_size];
   };
   ~hash() = default;
   // 哈希函数
@@ -30,7 +30,7 @@ class hash {
     int idx = hashfuc(key);
     // 扩容
     if (idx >= _size) {
-      Data<T>* temp = new Data<T>[idx + 1];
+      Data<T>** temp = new Data<T>*[idx + 1];
       for (int i = 0; i < _size; i++) {
         temp[i] = hashtable_[i];
       }
@@ -43,15 +43,16 @@ class hash {
       hashtable_[idx] = data;
     } else {  // key值发生冲突
       Data<T>* p = hashtable_[idx];
-      if (p->_back == nullptr) {
-        p->_back = data;
-        data->_forward = p;
+      if (p->_next == nullptr) {
+        p->_next = data;
+        data->_before = p;
+      } else {
+        while (p->_next != nullptr) {
+          p = p->_next;
+        }
+        p->_next = data;
+        data->_before = p;
       }
-      while (p->_back != nullptr) {
-        p = p->_back;
-      }
-      p->_back = data;
-      data._forward = p;
     }
   }
 
@@ -61,50 +62,52 @@ class hash {
       return;
     } else {
       Data<T>* p = hashtable_[idx];
-      if (p->_back == nullptr) {
+      if (p->_next == nullptr) {
         delete p;
       }
-      while (p->_back != nullptr) {
+      while (p->_next != nullptr) {
         if (p->_key == key) {
-          if (p->_forward == nullptr) {
-            hashtable_[idx] = p->_back;
-            p->_back->_forward = nullptr;
+          if (p->_before == nullptr) {
+            hashtable_[idx] = p->_next;
+            p->_next->_before = nullptr;
             delete p;
             break;
-          } else if (p->_back == nullptr) {
-            p->_forward->_back == nullptr;
+          } else if (p->_next == nullptr) {
+            p->_before->_next == nullptr;
             delete p;
             break;
           } else {
-            p->_back->_forward = p->_forward;
-            p->_forward->_back = p->_back;
+            p->_next->_before = p->_before;
+            p->_before->_next = p->_next;
             delete p;
             break;
           }
         }
-        p = p->_back;
+        p = p->_next;
       }
     }
   }
   T find(const int key) {
     int idx = hashfuc(key);
-    if (hashtable_[idx] == nullptr) {
-      throw "not found key" + key;
-    } else {
+    if (hashtable_[idx] != nullptr) {
       Data<T>* p = hashtable_[idx];
-      if (p->_back == nullptr) {
+      if (p->_next == nullptr) {
         return p->_value;
       }
-      while (p->_back != nullptr) {
-        if (p->_key = key) {
+      while (p != nullptr) {
+        if (p->_key == key) {
           return p->_value;
         }
+        p = p->_next;
       }
+    } else {
+      throw "not found key" + std::to_string(key);
     }
+    return {};
   }
 
  private:
-  Data<T>* hashtable_;
+  Data<T>** hashtable_;
   int _base;
   int _size;
 };
--- hash/test.cpp
+++ hash/test.cpp
@@ -4,15 +4,27 @@
 #include "hash.h"
 
 int main() {
-  __HASH::hash<std::string> test0;
-  test0.insert(2, "wxj");
-  test0.insert(10, "cx");
-  test0.insert(43, "bst");
-  test0.insert(65, "sjf");
+  try {
+    __HASH::hash<std::string> test0;
+    std::cout << "----------------insert test--------" << std::endl;
+    test0.insert(2, "wxj");
+    test0.insert(5, "cx");
+    test0.insert(43, "bst");
+    test0.insert(65, "sjf");
 
-  std::cout << test0.find(2) << std::endl;
-  std::cout << test0.find(10) << std::endl;
-  std::cout << test0.find(43) << std::endl;
-  std::cout << test0.find(65) << std::endl;
+    std::cout << test0.find(2) << std::endl;
+    std::cout << test0.find(5) << std::endl;
+    std::cout << test0.find(43) << std::endl;
+    std::cout << test0.find(65) << std::endl;
+
+    std::cout << "----------------remove test--------" << std::endl;
+    test0.remove(65);
+    std::cout << test0.find(2) << std::endl;
+    std::cout << test0.find(43) << std::endl;
+    std::cout << test0.find(5) << std::endl;
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << '\n';
+  }
+
   return 0;
 }
